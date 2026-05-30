e2de596725563d0cb0284dd2b0eeed71648729ea
LuizChristani <luizchristani159@gmail.com>
2026-05-30 12:49:20 -0300

    fix: add labels for normalization

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -1,6 +1,25 @@
 import type { TaskProps } from "../types/Task"
 import { TaskItem } from "./TaskItem"
 
+const TimeEnum = {
+    Manha: "manha",
+    Tarde: "tarde",
+    Noite: "noite"
+} as const 
+
+type TimeEnum = typeof TimeEnum[keyof typeof TimeEnum]
+
+const labels: Record<TimeEnum, string> = {
+    [TimeEnum.Manha]: "Manhã",
+    [TimeEnum.Tarde]: "Tarde",
+    [TimeEnum.Noite]: "Noite",
+}
+
 export const Task = ({ task, time }: TaskProps) => {
-    return <div className="flex flex-col space-y-3 w-full"><TaskItem task={task} time={time}/></div>
+    return (
+        <div className="flex flex-col space-y-3 w-full">
+            <span className="text-lg font-semibold">{labels[time.toLowerCase() as TimeEnum]}</span>
+            <TaskItem task={task} time={time}/>
+        </div>
+    )
 }
--- frontend/src/constants/Task.ts
+++ frontend/src/constants/Task.ts
@@ -4,63 +4,63 @@ const TASKS = [
         titulo: 'Ler',
         detalhes: "Ler livro",
         status: "concluida",
-        time: "manha"
+        time: "Manha"
     },
     {
         id: 2,
         titulo: 'Estudar',
         detalhes: "Estudar FSC",
         status: "concluida",
-        time: "manha"
+        time: "Manha"
     },
     {
         id: 3,
         titulo: 'Ler',
         detalhes: "Ler livro",
         status: "concluida",
-        time: "manha"
+        time: "Manha"
     },
     {
         id: 4,
         titulo: 'Ler',
         detalhes: "Ler livro",
         status: "pendente",
-        time: "tarde"
+        time: "Tarde"
     },
     {
         id: 5,
         titulo: 'Estudar',
         detalhes: "Estudar FSC",
         status: "pendente",
-        time: "tarde"
+        time: "Tarde"
     },
     {
         id: 6,
         titulo: 'Ler',
         detalhes: "Ler livro",
         status: "pendente",
-        time: "tarde"
+        time: "Tarde"
     },
     {
         id: 7,
         titulo: 'Ler',
         detalhes: "Ler livro",
         status: "progresso",
-        time: "noite"
+        time: "Noite"
     },
     {
         id: 8,
         titulo: 'Estudar',
         detalhes: "Estudar FSC",
         status: "progresso",
-        time: "noite"
+        time: "Noite"
     },
     {
         id: 9,
         titulo: 'Ler',
         detalhes: "Ler livro",
         status: "progresso",
-        time: "noite"
+        time: "Noite"
     }
 ]
 
